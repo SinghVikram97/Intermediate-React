15b2c157853f8069af05ffe816b4f8f093bdbf35
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-07-27 11:24:37 +0530

    Setting style guide and selectors

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
+// You can have all your design themes and standards in one place
+import colors from "./colors";
+
 const Navbar = () => {
   const [padding, setPadding] = useState(15);
   return (
     /*eslint-disable */
     <header
       css={css`
-        background-color: pink;
+        background-color: ${colors.secondary};
         padding: ${padding}px;
       `}
       onClick={() => setPadding(padding + 15)}
@@ -17,6 +20,10 @@ const Navbar = () => {
       <span
         css={css`
           font-size: 60px;
+          /* & refers to the actual component itself */
+          &:hover {
+            text-decoration: underline;
+          }
         `}
         role="img"
         aria-label="logo"
