0bef78a4345c7f4a95a9cc9f5c22dd6b94bb306b
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-07-27 11:11:41 +0530

    change css onClick

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
 const Navbar = () => {
+  const [padding, setPadding] = useState(15);
   return (
+    /*eslint-disable */
     <header
       css={css`
-        background-color: #333;
-        padding: 15px;
+        background-color: pink;
+        padding: ${padding}px;
       `}
+      onClick={() => setPadding(padding + 15)}
     >
       <Link to="/">Adopt Me</Link>
-      <span role="img" aria-label="logo">
+      <span
+        css={css`
+          font-size: 60px;
+        `}
+        role="img"
+        aria-label="logo"
+      >
         🐩
       </span>
     </header>
