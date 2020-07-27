8c2487fd011c23b061b54fb5207c62b7cc1f5245
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-07-27 13:58:21 +0530

    Lazy loading a component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
-import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+
+// We don't want to include details page right away
+// But as soon as parcel/webpack see this import it will import and bundle it in
+// SO we delete it from here
+
+// import Details from "./Details";
+
+const Details = lazy(() => import("./Details.js"));
+// Details is now a placeholder component that will not load the code
+// Untill later when Details is actually tried to be rendered for the 1st time
 
 const App = () => {
   const theme = useState("darkblue");
@@ -13,10 +22,14 @@ const App = () => {
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        {/* Need to use Suspense component with lazy */}
+        <Suspense fallback={<h1>Loading Route....</h1>}>
+          {/* Fallback in case code not ready yet */}
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
