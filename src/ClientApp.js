1ecb7aee2ca218f907574c8e494274fff337bffc
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-07-28 12:15:26 +0530

    Added express and ssr works

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -4,4 +4,14 @@ import App from "./App";
 
 // Other browser only things
 
+// Hydrate is like render
+// But
+// Hydrate is basically used in case of SSR(Server side Rendering).
+// SSR gives you the skeleton or HTML markup which is being shipped from a server
+// so that for the first time when your page loads it is not blank and s
+// earch engine bots can index it for SEO(A use case of SSR).
+// So hydrate adds the JS to your page or a node to which SSR is applied. S
+// So that your page responds to the events performed by the user.
+
+// Whereas render blows away whatevers there and re-render it
 hydrate(<App />, document.getElementById("root"));
